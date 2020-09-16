9625f8893d859ec1f47de887d79b1d328268c118
priyaViveka <shanmown@gmail.com>
2020-09-16 11:25:45 +0530

    first program

---
+++ src/org/cmp/AxisBank.java
@@ -0,0 +1,14 @@
+package org.cmp;
+
+public class AxisBank extends BankInfo{
+public void saving() {
+	// TODO Auto-generated method stub
+System.out.println("hgugv");
+}
+public static void main(String[] args) {
+	AxisBank s=new AxisBank();
+	s.saving();
+	s.deposit();
+	s.fied();
+}
+}
---
+++ src/org/cmp/BankInfo.java
@@ -0,0 +1,16 @@
+package org.cmp;
+
+public class BankInfo {
+public void saving() {
+	// TODO Auto-generated method stub
+System.out.println("my saving is less");
+}
+public void deposit() {
+	// TODO Auto-generated method stub
+System.out.println("hghgh");
+}
+public void fied() {
+	// TODO Auto-generated method stub
+System.out.println("ffffuf");
+}
+}
